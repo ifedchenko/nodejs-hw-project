dd5db9f2f20d182885bce9f97b4435e248e22fc5
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-09-15 20:24:01 +0300

    add mongoose connection

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
 
-    // "start": "cross-env NODE_ENV=production node ./server.js",
-    // "start:dev": "cross-env NODE_ENV=development nodemon ./server.js",
+const user = "yfn";
+const password = "nenDQx8b8N2rnrLO";
+const DB_HOST = `mongodb+srv://${user}:${password}@cluster0.yc88dmg.mongodb.net/notebook`;
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch(error => {
+    console.log("DB error", error.message);
+    process.exit(1);
+  });
