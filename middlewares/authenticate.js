d32e6350835342bab7ed5a1e1aaff514b8751dd5
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-09-23 22:05:21 +0300

    Middleware token authorization

---
+++ middlewares/authenticate.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/user");
+const { HttpError } = require("../utils");
+require("dotenv").config();
+const { SECRET_KEY } = process.env;
+
+const authenticate = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+
+  if (bearer !== "Bearer") {
+    next(HttpError(401, "Not authorized"));
+  }
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user) {
+      next(HttpError(401, "User not found"));
+    }
+    next();
+  } catch (error) {
+    next(HttpError(401, "Not authorized"));
+  }
+};
+
+module.exports = authenticate;
