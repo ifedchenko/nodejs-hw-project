88f3dfb70dfc07ab630580164bf0eb281ef0b013
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-09-23 20:12:20 +0300

    add login route and controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,9 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../utils");
+require("dotenv").config();
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res, next) => {
   const { email, password } = req.body;
@@ -21,6 +24,34 @@ const register = async (req, res, next) => {
   });
 };
 
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  res.status(200).json({
+    token: token,
+    user: {
+      email: email,
+      subscription: "starter",
+    },
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,14 +1,21 @@
 const express = require("express");
+
 const router = express.Router();
 
 const { validateBody } = require("../../middlewares");
+
 const { schemas } = require("../../models/user");
+
 const ctrl = require("../../controllers/auth");
 
+// signup
 router.post(
   "/register",
   validateBody(schemas.registrationSchema),
   ctrl.register
 );
+
+// signin
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 module.exports = router;
