4bb9af5aeaeea89b60266143c989eb9a2650c087
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-10-03 21:35:53 +0300

    update  mongooseScema and add emailVerification schema

--- models/user.js
+++ models/user.js
@@ -31,6 +31,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false, timestamps: true }
 );
@@ -53,9 +61,14 @@ const loginSchema = Joi.object({
   // owner: Joi.string(),
 });
 
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
 const schemas = {
   registrationSchema,
   loginSchema,
+  emailSchema,
 };
 const User = model("user", userSchema);
 
